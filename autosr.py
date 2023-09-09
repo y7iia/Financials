bc7445d77c9d8acce76f1b877626ad296d761bb7
Yahia A Telmisany <90855233+y7iia@users.noreply.github.com>
2023-09-09 22:15:21 +0300

    Update autosr.py
    adding plot

--- autosr.py
+++ autosr.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import yfinance as yf
+import plotly.graph_objects as go
 
 def calculate_pivot_points(high, low, close, method):
     if method == "standard":
@@ -61,3 +62,35 @@ if st.button('أحسب Calculate Pivot Points'):
     st.write(f"=====S1 is: {S1:.2f}")
     st.write(f"==========S2 is: {S2:.2f}")
     st.write(f"===============S3 is: {S3:.2f}")
+
+
+# get historical market data for the last week
+data = yf.download(ticker, period="1wk")
+
+fig = go.Figure(data=[
+    go.Candlestick(x=data.index,
+                   open=data['Open'],
+                   high=data['High'],
+                   low=data['Low'],
+                   close=data['Close'])
+])
+
+# Add support and resistance levels
+fig.add_trace(go.Scatter(x=data.index, y=[R1]*len(data.index), mode='lines', name='R1'))
+fig.add_trace(go.Scatter(x=data.index, y=[R2]*len(data.index), mode='lines', name='R2'))
+fig.add_trace(go.Scatter(x=data.index, y=[R3]*len(data.index), mode='lines', name='R3'))
+fig.add_trace(go.Scatter(x=data.index, y=[S1]*len(data.index), mode='lines', name='S1'))
+fig.add_trace(go.Scatter(x=data.index, y=[S2]*len(data.index), mode='lines', name='S2'))
+fig.add_trace(go.Scatter(x=data.index, y=[S3]*len(data.index), mode='lines', name='S3'))
+
+# Set titles
+fig.update_layout(
+    title=f'Candlestick chart for {ticker}',
+    yaxis_title='Stock Price (Currency)',
+    shapes = [dict(x0='2019-12-01', x1='2019-12-01', y0=0, y1=1, xref='x', yref='paper',
+                   line_width=2)], # adding a vertical line
+    annotations=[dict(x='2019-12-01', y=0.05, xref='x', yref='paper',
+                      showarrow=False, xanchor='left', text='Increase Period Begins')]
+)
+
+st.plotly_chart(fig)
