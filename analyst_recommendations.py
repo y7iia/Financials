7154b93033f630d7bd35827b18f68177af89c874
Yahia A Telmisany <90855233+y7iia@users.noreply.github.com>
2023-09-12 12:45:22 +0300

    Update analyst_recommendations.py

--- analyst_recommendations.py
+++ analyst_recommendations.py
@@ -8,51 +8,53 @@ st.markdown('برمجة يحيى التلمساني - @telmisany')
 # Define the ticker code
 code = st.text_input('ادخل رمز السهم')
 
-if code:
-    # Check if the ticker is numeric and append '.SR' if it is
-    if code.isdigit():
-        code = code + '.SR'
+# Add a button for the user to click when they are ready to submit
+if st.button('Submit'):
+    if code:
+        # Check if the ticker is numeric and append '.SR' if it is
+        if code.isdigit():
+            code = code + '.SR'
 
-    # Fetch data
-    ticker = yf.Ticker(code)
-    info = ticker.info
+        # Fetch data
+        ticker = yf.Ticker(code)
+        info = ticker.info
 
-    # Convert the data into a DataFrame
-    df = pd.DataFrame.from_dict(info, orient='index').T
+        # Convert the data into a DataFrame
+        df = pd.DataFrame.from_dict(info, orient='index').T
 
-    # Define the columns to be selected
-    col = ['longBusinessSummary','recommendationKey','currentPrice','targetHighPrice','targetMeanPrice','targetLowPrice','numberOfAnalystOpinions']
+        # Define the columns to be selected
+        col = ['longBusinessSummary','recommendationKey','currentPrice','targetHighPrice','targetMeanPrice','targetLowPrice','numberOfAnalystOpinions']
 
-    # Define the dictionary for renaming the columns
-    col_dict = {
-    'longBusinessSummary': 'الشركة',
-    'recommendationKey': 'توصيات المحللين',
-    'currentPrice': 'السعر الحالي',
-    'targetHighPrice': 'أعلى سعر مستهدف',
-    'targetMeanPrice': 'متوسط السعر المستهدف',
-    'targetLowPrice': 'أدني سعر مستهدف',
-    'numberOfAnalystOpinions': 'عدد آراء المحللين'
-    }
+        # Define the dictionary for renaming the columns
+        col_dict = {
+        'longBusinessSummary': 'الشركة',
+        'recommendationKey': 'توصيات المحللين',
+        'currentPrice': 'السعر الحالي',
+        'targetHighPrice': 'أعلى سعر مستهدف',
+        'targetMeanPrice': 'متوسط السعر المستهدف',
+        'targetLowPrice': 'أدني سعر مستهدف',
+        'numberOfAnalystOpinions': 'عدد آراء المحللين'
+        }
 
-    # Select the columns from the DataFrame
-    selected_df = df[col]
+        # Select the columns from the DataFrame
+        selected_df = df[col]
 
-    # Rename the columns
-    renamed_df = selected_df.rename(columns=col_dict)
+        # Rename the columns
+        renamed_df = selected_df.rename(columns=col_dict)
 
-    # Print the renamed DataFrame + Transpose the data
-    table = renamed_df.T
-    table['%'] = '-'
+        # Print the renamed DataFrame + Transpose the data
+        table = renamed_df.T
+        table['%'] = '-'
 
-    for i , n in enumerate(table[0]):
-        try:
-            if  round((table[0][i] / table[0][2]-1)*100,2) > 0:
-                table['%'][i] = '+' + str(round((table[0][i] / table[0][2]-1)*100,1)) + '%'
-            else:
-                table['%'][i] = str(round((table[0][i] / table[0][2]-1)*100,1)) + '%'
-        except:
-            continue
+        for i , n in enumerate(table[0]):
+            try:
+                if  round((table[0][i] / table[0][2]-1)*100,2) > 0:
+                    table['%'][i] = '+' + str(round((table[0][i] / table[0][2]-1)*100,1)) + '%'
+                else:
+                    table['%'][i] = str(round((table[0][i] / table[0][2]-1)*100,1)) + '%'
+            except:
+                continue
 
-    table['%'][6] = '-' 
-    table['%'][2] = '-'
-    st.dataframe(table)
+        table['%'][6] = '-' 
+        table['%'][2] = '-'
+        st.dataframe(table)
