1a82b6495cea85d3b0f8c345dcd2add6cf833cc7
Yahia A Telmisany <90855233+y7iia@users.noreply.github.com>
2023-09-17 23:37:33 +0300

    Update graham_number.py

--- graham_number.py
+++ graham_number.py
@@ -291,23 +291,27 @@ def calculate_graham_number_and_eps_type(row, factor):
         logging.error(f"Error calculating Graham number for row {row['symbol']}: {e}")
         return np.nan, None
 
-def get_data_for_sector(sector):
+def get_data_for_sector(sector, companies):
     try:
         stock_codes = tasi[sector]
         data = [fetch_data_for_stock(code) for code in stock_codes]
         df = pd.concat(data, ignore_index=True)
-        columns_to_select = ['trailingEps','forwardEps','bookValue']
+        columns_to_select = ['symbol','shortName','trailingEps','forwardEps','bookValue']
         df = df[[col for col in columns_to_select if col in df.columns]]
         # Add 'company' column
+        df['company'] = df['symbol'].copy()
         # Replace symbols with company names
-        df['Company'] = df['symbol'].replace(companies)   
+        df['symbol'] = df['symbol'].replace(companies)
+        # Remove 'shortName' column
+        df = df.drop('shortName', axis=1)
         # Calculate Graham numbers and add new columns
         for factor in [22.5, 30, 50]:
             df[f'Graham_{factor}'], df['EPS_Type'] = zip(*df.apply(lambda row: calculate_graham_number_and_eps_type(row, factor), axis=1))
         return df
     except Exception as e:
-        logging.error(f"Error getting data for sector {sector}: {
-
+        logging.error(f"Error getting data for sector {sector}: {e}")
+        return pd.DataFrame()
+     
 # Streamlit code
 st.title('Financials Analysis Application')
 
