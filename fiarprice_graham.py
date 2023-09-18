9fb39daea971cb8f1a74dfc588f66f7989c18040
Yahia A Telmisany <90855233+y7iia@users.noreply.github.com>
2023-09-18 13:02:15 +0300

    Update fiarprice_graham.py

--- fiarprice_graham.py
+++ fiarprice_graham.py
@@ -1,9 +1,35 @@
 import streamlit as st
-import numpy as np
 import pandas as pd
-from yahooquery import Ticker
+import yfinance as yf
+import numpy as np
+import logging
 
-dict_df = {'2222.SR': 'أرامكو السعودية',
+#check the tasi dic again later
+tasi = {
+'البتروكيماويات': ['2350.SR',  '1211.SR',  '2310.SR',  '2380.SR',  '2001.SR',  '2330.SR',  '2060.SR',  '2010.SR',  '2170.SR',  '2250.SR',  '2290.SR',  '2210.SR',  '1210.SR',  '2020.SR',  '2223.SR'],
+'الأسمنت': ['3020.SR',  '3040.SR',  '3030.SR',  '3090.SR',  '3091.SR',  '3005.SR',  '3004.SR',  '3003.SR',  '3080.SR',  '3050.SR',  '3010.SR',  '3002.SR',  '3001.SR',  '3060.SR'],
+'البنوك': ['1180.SR',  '1010.SR',  '1150.SR',  '1050.SR',  '1140.SR',  '1020.SR',  '1182.SR',  '1120.SR',  '1030.SR',  '1080.SR',  '1060.SR',  '1183.SR'],
+'التأمين': ['8210.SR',  '8250.SR',  '8030.SR',  '8160.SR',  '8280.SR',  '8240.SR',  '8070.SR',  '8120.SR',  '8170.SR',  '8270.SR',  '8050.SR',  '8100.SR',  '8012.SR',  '8150.SR',  '8311.SR',  '8260.SR',  '8200.SR',  '8310.SR',  '8180.SR',  '8060.SR',  '8312.SR',  '8190.SR',  '8020.SR',  '8040.SR',  '8300.SR',  '8230.SR',  '8010.SR'],
+'الرعاية الصحية': ['4004.SR',  '4005.SR',  '4007.SR',  '4013.SR',  '4009.SR',  '4002.SR',  '2140.SR',  '2230.SR',  '4014.SR'],
+'التطبيقات وخدمات التقنية': ['7203.SR','7201.SR','7200.SR',  '7202.SR',  '7204.SR'],  
+'الإتصالات': ['7020.SR', '7040.SR', '7030.SR', '7010.SR'],
+'الإستثمار والتمويل': ['1111.SR',  '4280.SR',  '4080.SR',  '4081.SR',  '4130.SR',  '2120.SR',  '4082.SR'],
+'إدارة وتطوير العقارات': ['4300.SR',  '4310.SR',  '4020.SR',  '4150.SR',  '4230.SR',  '4320.SR',  '4100.SR',  '4090.SR',  '4321.SR',  '4250.SR',  '4220.SR',  '4322.SR',  '4323.SR'],
+'الأدوية': ['2070.SR', '4015.SR'], 
+'الإعلام والترفيه': ['4210.SR', '4070.SR', '4071.SR'],
+'الخدمات الإستهلاكية': ['1810.SR',  '1830.SR',  '4291.SR',  '4290.SR',  '6012.SR','6013.SR','1820.SR',  '4292.SR',  '6002.SR',  '4010.SR',  '4170.SR','6014.SR', '6015.SR'],
+'الخدمات التجارية والمهنية': ['4270.SR',  '1831.SR',  '6004.SR',  '1832.SR',  '1833.SR'],
+'السلع الرأسمالية': ['2040.SR',  '1303.SR',  '1212.SR',  '2110.SR',  '2370.SR',  '2160.SR',  '4141.SR',  '2360.SR',  '1302.SR',  '2320.SR',  '4140.SR',  '4142.SR'],
+'السلع طويلة الاجل': ['2340.SR','4180.SR',  '4012.SR',  '2130.SR',  '1213.SR',  '4011.SR'],
+'المرافق العامة': ['2082.SR', '5110.SR', '2080.SR', '2081.SR', '2083.SR'],
+'المواد الأساسية': ['1202.SR',  '2300.SR',  '1320.SR',  '1201.SR',  '3008.SR', '2220.SR',  '2200.SR',  '1301.SR',  '3007.SR',  '2180.SR',  '2240.SR',  '1321.SR',  '1304.SR',  '2090.SR',  '2150.SR',  '1322.SR'], 
+'النقل': ['4261.SR', '4260.SR', '4031.SR', '4040.SR', '4110.SR', '2190.SR'],
+'انتاج الأغذية': ['2280.SR',  '2050.SR',  '2270.SR',  '6001.SR',  '6020.SR',  '6090.SR',  '6010.SR',  '2281.SR',  '6070.SR',  '2100.SR',  '6060.SR',  '6050.SR',  '6040.SR', '2282.SR',  '2283.SR'],
+'تجزئة الأغذية': ['4161.SR',  '4001.SR',  '4162.SR',  '4160.SR',  '4061.SR',  '4006.SR',  '4163.SR',  '4164.SR'], 
+'تجزئة السلع الكمالية': ['4003.SR',  '4190.SR',  '4191.SR',  '1214.SR',  '4008.SR','4240.SR',  '4050.SR',  '4051.SR',  '4192.SR'],
+}
+
+companies = {'2222.SR': 'أرامكو السعودية',
  '1180.SR': 'الأهلي السعودي',
  '2082.SR': 'أكوا باور',
  '1010.SR': 'الرياض',
@@ -230,56 +256,118 @@ dict_df = {'2222.SR': 'أرامكو السعودية',
  '1833.SR': 'الموارد',
  '4015.SR': 'جمحوم فارما',
  '4082.SR': 'مرنة',
- '2283.SR': 'المطاحن الأولى'}
+ '2283.SR': 'المطاحن الأولى',
+ '4323.SR': 'سمو'}
 
-st.title('الأرباح المبقاة Retained Income')
+# Setup logging
+logging.basicConfig(filename='app.log', level=logging.ERROR)
+
+def fetch_data_for_stock(stock):
+    try:
+        # Fetch data for a stock using yfinance
+        data = yf.Ticker(stock).info
+        # Convert the dictionary into a DataFrame
+        df = pd.DataFrame([data])
+        return df
+    except Exception as e:
+        logging.error(f"Error fetching data for stock {stock}: {e}")
+        return pd.DataFrame()
+
+def calculate_graham_number_and_eps_type(row, factor):
+    try:
+        # Use forward EPS if available, else use trailing EPS
+        if not pd.isna(row['forwardEps']):
+            eps = row['forwardEps']
+            eps_type = 'Forward'
+        else:
+            eps = row['trailingEps']
+            eps_type = 'Trailing'
+        # Calculate Graham number and round to 2 decimal places
+        graham_number = round(np.sqrt(factor * eps * row['bookValue']), 2)
+        return graham_number, eps_type
+    except Exception as e:
+        logging.error(f"Error calculating Graham number for row {row['symbol']}: {e}")
+        return np.nan, None
+
+def get_data_for_sector(sector):
+    try:
+        stock_codes = tasi[sector]
+        data = [fetch_data_for_stock(code) for code in stock_codes]
+        df = pd.concat(data, ignore_index=True)
+        columns_to_select = ['symbol','trailingEps','forwardEps','bookValue', 'currentPrice']
+        df = df[[col for col in columns_to_select if col in df.columns]]
+        # Add 'company' column
+        df['company'] = df['symbol'].copy()
+        # Replace symbols with company names
+        df['symbol'] = df['symbol'].replace(companies)
+        # Calculate Graham numbers and add new columns
+        for factor in [22.5, 30, 50]:
+            df[f'Graham_{factor}'], df['EPS_Type'] = zip(*df.apply(lambda row: calculate_graham_number_and_eps_type(row, factor), axis=1))
+        # Drop rows with missing 'Graham_22.5' values
+        df = df.dropna(subset=['Graham_22.5'])
+        # Reorder columns
+        df = df[['symbol', 'company', 'trailingEps', 'forwardEps', 'bookValue', 'currentPrice', 'Graham_22.5', 'Graham_30', 'Graham_50']]
+        # Set 'symbol' as index
+        df = df.set_index('symbol')
+        # Rename index
+        df.index.names = ['الشركة']
+        # Rename columns
+        column_names = {
+            'company': 'الرمز',
+            'trailingEps': 'ربحية السهم الحالية',
+            'forwardEps': 'ربحية السهم المتوقعة',
+            'bookValue': 'القيمة الدفترية',
+            'currentPrice': 'السعر الحالي',
+            'Graham_22.5': 'تقييم متشدد',
+            'Graham_30': 'تقييم متساهل',
+            'Graham_50': 'تقييم متساهل جدا'
+        }
+        df = df.rename(columns=column_names)
+        # Round all floating-point numbers to two decimal places
+        df = df.round({col: 2 for col in df.select_dtypes(float).columns})
+        # Apply styling
+        def color_cells(row):
+            color = {}
+            for col in ['تقييم متشدد', 'تقييم متساهل', 'تقييم متساهل جدا']:
+                color[col] = 'background-color: LightGreen' if row['السعر الحالي'] < row[col] else 'background-color: LightCoral'
+            return pd.Series(color)
+        styled_df = df.style.apply(color_cells, axis=1)
+        return styled_df
+    except Exception as e:
+        logging.error(f"Error getting data for sector {sector}: {e}")
+        return pd.DataFrame()
+     
+     
+# Streamlit code
+st.title('حساب القيمة العادلة بأستخدام طريقة جراهام')
 st.markdown(' @telmisany - برمجة يحيى التلمساني')
 
-def get_retained_earnings(ticker):
-    code = Ticker(ticker)
-    code = code.all_financial_data(frequency = 'q').reset_index()
-    code['RetainedEarnings'] = code['RetainedEarnings'] / 1000000
 
-    # Rename the column
-    code = code.rename(columns={'RetainedEarnings': 'Retained Income الأرباح المبقاة بالمليون',
-                                'symbol': 'الرمز: symbol',
-                                'asOfDate': ' Date : التاريخ' })
+# User input
+sector = st.selectbox('اختار القطاع المطلوب', options=[''] + list(tasi.keys()))
 
-    data = code[['الرمز: symbol',' Date : التاريخ','Retained Income الأرباح المبقاة بالمليون']]
-    if '.SR' in ticker or ticker.isdigit():
-      data['الشركة : Company'] = dict_df[ticker]
+# Submit button
+if st.button('Submit'):
+    if sector:
+        # Fetch and display data
+        sector_data = get_data_for_sector(sector)
+        st.dataframe(sector_data)
     else:
-      data['الشركة : Company'] = ticker
+        st.write(":أختار القطاع المطلوب")
 
-    data = data[['الشركة : Company', 'الرمز: symbol',' Date : التاريخ','Retained Income الأرباح المبقاة بالمليون']].copy()
-    return data
-
-ticker_input = st.text_input("ادخل رمز الشركة:")
-
-if st.button("Submit"):
-    if ticker_input:
-        if ticker_input.isdigit():
-            ticker_input = int(ticker_input)
-            if 999 <= ticker_input <= 9999:
-                ticker = str(ticker_input) + '.SR'
-            else:
-                st.write("ادخل رمز الشركة: ")
-        else:
-            ticker = ticker_input
-
-        df = get_retained_earnings(ticker)
-        st.dataframe(df)
-
+# Fetch and display data
+sector_data = get_data_for_sector(sector)
 
 
 st.write('\n')
 st.markdown('[أنظر ايضا: آراء المحللين](https://twitter.com/telmisany/status/1701640774138445878)')
 st.write('\n')
 st.markdown('[أنظر ايضا: حاسبة الدعوم والمقاومات](https://twitter.com/telmisany/status/1700897237096640791)')
+st.write('\n')
+st.markdown('[أنظر ايضا: الأرباح المبقاة](https://twitter.com/telmisany/status/1700128870349811959)')
 
 # Add three empty lines for spacing
 st.write('\n\n\n')
 
 # Add a hyperlink to your Twitter account
 st.markdown('[X تابعني في منصة](https://twitter.com/telmisany)')
-
