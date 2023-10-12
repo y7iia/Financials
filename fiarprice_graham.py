fc966648ebebdeccd3d9c74b28504965e8a0ba4f
Yahia A Telmisany <90855233+y7iia@users.noreply.github.com>
2023-10-13 01:03:02 +0300

    Update fiarprice_graham.py

--- fiarprice_graham.py
+++ fiarprice_graham.py
@@ -365,18 +365,10 @@ if st.button('Submit'):
 sector_data = get_data_for_sector(sector)
 
 
-st.write('\n')
-st.markdown('[أنظر ايضا: آراء المحللين](https://twitter.com/telmisany/status/1701640774138445878)')
-st.write('\n')
-st.markdown('[أنظر ايضا: حاسبة الدعوم والمقاومات](https://twitter.com/telmisany/status/1700897237096640791)')
-st.write('\n')
-st.markdown('[أنظر ايضا: الأرباح المبقاة](https://twitter.com/telmisany/status/1700128870349811959)')
-
+st.markdown('[تطبيقات أخرى قد تعجبك](https://twitter.com/telmisany/status/1702641486792159334)')
 # Add three empty lines for spacing
 st.write('\n\n\n')
-
 # Add a hyperlink to your Twitter account
 st.markdown('[X تابعني في منصة](https://twitter.com/telmisany)')
 st.markdown('[قهويني؟](https://www.buymeacoffee.com/y7iia)')
-
 
