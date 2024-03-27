cd9317a80d8495d86bf5a2401480fca0eb86e6e1
Yahia A Telmisany <90855233+y7iia@users.noreply.github.com>
2024-03-28 00:37:46 +0300

    Update net_income.py

--- net_income.py
+++ net_income.py
@@ -307,13 +307,13 @@ if st.button("Submit"):
             # Fetch data
             df = aggregate_financial_data(tickers, frequency)
 
-            # Display data
-            if df is not None:
-                df.index.names = ['Ticker']
-                df = df.rename(index=companies)
-                st.write(df.T)
-            else:
-                st.error("تعذر جلب البيانات")
+            # # Display data
+            # if df is not None:
+            #     df.index.names = ['Ticker']
+            #     df = df.rename(index=companies)
+            #     st.write(df.T)
+            # else:
+            #     st.error("تعذر جلب البيانات")
 
 # Add a statement
 st.write("> **ملاحظة: جميع الأرقام بالمليون ريال سعودي**")
