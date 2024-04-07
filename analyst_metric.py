e39b19e6b3b6519a56a20283e395b7b5674b7289
Yahia A Telmisany <90855233+y7iia@users.noreply.github.com>
2024-04-07 23:05:14 +0300

    Update analyst_metric.py

--- analyst_metric.py
+++ analyst_metric.py
@@ -258,9 +258,9 @@ def evaluate_analyst_recommendation(ticker, target_date, target_price):
             'السهم': company_name,
             'الهدف': target_price,
             'تحقق الهدف ؟': 'نعم' if highest_price >= target_price else 'لا',
-            'أعلى سعر وصل له السهم و التاريخ': f"{highest_price} ({highest_date})",
-            'أعلى سعر وصل له السهم (مع العائد)': f"{highest_price} ({highest_return:.2f}%)",
-            'أقل سعر وصل له السهم( مع العائد)': f"{lowest_price} ({lowest_return:.2f}%)"
+            'أعلى سعر وصل له السهم و التاريخ': f"{highest_price:.2f} ({highest_date})",
+            'أعلى سعر وصل له السهم (مع العائد)': f"{highest_price:.2f} ({highest_return:.2f}%)",
+            'أقل سعر وصل له السهم( مع العائد)': f"{lowest_price:.2f} ({lowest_return:.2f}%)"
         }
         return pd.DataFrame([result_data])
     else:
