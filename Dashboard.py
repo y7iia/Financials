96ddd63c6d5539c58503199d326c81b6120bd12d
Yahia A Telmisany <90855233+y7iia@users.noreply.github.com>
2024-07-01 11:17:11 +0300

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -73,3 +73,13 @@ for i, (app_name, link) in enumerate(app_links):
                 f'</a>',
                 unsafe_allow_html=True,
             )
+
+# Add three empty lines for spacing
+st.write('\n\n\n')
+# Add a hyperlink to your Twitter account
+st.markdown('[X تابعني في منصة](https://twitter.com/telmisany)')
+
+# Buy me coffee AD:
+image_url = 'https://i.ibb.co/dM0tT0f/buy-me-coffee.png'
+link_url = 'https://www.buymeacoffee.com/y7iia'
+st.markdown(f'<a href="{link_url}"><img src="{image_url}" alt="Image" width="200"/></a>', unsafe_allow_html=True)
