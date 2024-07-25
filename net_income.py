bb96a56ff112c65646a75b0596f1230551163365
Yahia A Telmisany <90855233+y7iia@users.noreply.github.com>
2024-07-25 21:59:22 +0300

    Update net_income.py

--- net_income.py
+++ net_income.py
@@ -377,6 +377,6 @@ st.write("> **ملاحظة: جميع الأرقام بالمليون ريال س
 st.markdown('[تابعني على تويتر](https://twitter.com/telmisany)')
 
 # Buy me a coffee AD
-image_url = 'https://i.ibb.co/dM0tT0f/buy-me-coffee.png'
+image_url = 'https://i.ibb.co/WkHT8HP/buy-me-coffee.png'
 link_url = 'https://www.buymeacoffee.com/y7iia'
 st.markdown(f'<a href="{link_url}"><img src="{image_url}" alt="Buy me a coffee" width="200"/></a>', unsafe_allow_html=True)
